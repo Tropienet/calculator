1ca5540f70652d18c97d27decae87656e687119b
Tropie <tingoricki1@gmail.com>
2022-01-03 20:35:07 +0100

    added operators

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
 const display = document.querySelector("#display");
 const numbersSelector = document.querySelectorAll(".number");
+const operatorsSelector = document.querySelectorAll(".operator");
+const clear = document.querySelector(".clear");
+let currentDisplayValue = 0;
+let storedDisplayValue = 0;
+let displayValueAfterOperation = 0;
+let currentOperator = "none";
 
-
+clear.addEventListener('click', () => {
+    currentDisplayValue = 0;
+    storedDisplayValue = 0;
+    displayValueAfterOperation = 0;
+    currentOperator = "none";
+    display.textContent = 0;
+})
 
 Array.from(numbersSelector).forEach(number => {
     number.addEventListener('click', () => {
-       display.textContent += number.textContent;
+        if(parseInt(display.textContent)==0){
+            display.textContent = number.textContent;
+            storeDisplayValue();
+        }else{
+            display.textContent += number.textContent;
+            storeDisplayValue();
+        }
     });
 });
+
+Array.from(operatorsSelector).forEach(operator => {
+    operator.addEventListener('click', () => {
+        checkCurrentOperator(operator);
+        
+    })
+})
 
 
 function add(a,b){
@@ -47,4 +72,41 @@ function operate(operator,a,b){
     }
     
     return result;
+    /*console.log(result);
+  storedDisplayValue += result;
+   display.textContent = storedDisplayValue; */
+}
+
+function storeDisplayValue(){
+   // currentDisplayValue = parseInt(display.textContent);
+}
+
+function checkCurrentOperator(operator){
+        if(currentOperator!="none"){
+            currentOperator = operator.textContent;
+            
+            let currentVal = operate(operator.textContent,  storedDisplayValue, currentDisplayValue);
+            storedDisplayValue = currentVal;
+            display.textContent = storedDisplayValue;
+            console.log(currentOperator);
+            console.log(currentVal);
+           // display.textContent = 0;
+            
+        }else if(currentOperator == "none"){
+            storedDisplayValue = currentDisplayValue;
+            display.textContent = 0;
+            currentOperator = operator.textContent;
+        }
+        
+      /*  if(currentOperator!="none"){
+          displayValueAfterOperation = operate(currentOperator, storedDisplayValue, currentDisplayValue);
+          display.textContent = 0;
+            
+        }else if(currentOperator=="none"){
+            currentOperator = operator.textContent;
+            storedDisplayValue += currentDisplayValue;
+           display.textContent = 0;
+        }*/
+
+        
 }
